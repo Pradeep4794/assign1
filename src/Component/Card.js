0f9067498e63d0e34944348c2fb8708aec969ffc
Pradeep4794 <pk676108@gmail.com>
2022-03-10 10:18:16 +0530

    first commit

---
+++ src/Component/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Cards from "./Cards";
+
+const Card = () => {
+  return (
+    <>
+      <div className="container mt-5">
+        <h2 className="text-center">Skip the line</h2>
+        <p className="text-center">
+          Buy your tickets or classess online and head
+          <br />
+          write to the rink when you get here
+        </p>
+        <div className="row ">
+          <div className="col-md-3 ">
+            <Cards />
+           
+          </div>
+
+           <div className="col-md-3 ">
+            <Cards />
+          </div>
+          <div className="col-md-3 ">
+            <Cards />
+          </div>
+          <div className="col-md-3 ">
+            <Cards />
+          </div> 
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
---
+++ src/Component/Cards.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+const Cards = () => {
+  return (
+    <>
+         
+            <div className="card" >
+              <img src="./Blog.jpg" className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make 
+                  
+                </p>
+                <p>$ 245</p>
+                <a href="#" className="btn btn-primary">
+                Add to Cart
+                </a>
+              </div>
+            </div>
+        
+    </>
+  )
+}
+
+export default Cards
